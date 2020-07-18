bd6d40f40634d142f99dea0ac9ce28f68e5c09bc
윤형석 <bagzein@gmail.com>
2020-07-18 14:16:53 +0900

    카카오 나에게 메시지 보내기

--- src/main/java/com/example/practice/BatchJob/kakaoAPI.java
+++ src/main/java/com/example/practice/BatchJob/kakaoAPI.java
@@ -1,10 +1,49 @@
 package com.example.practice.BatchJob;
 
+import com.squareup.okhttp.*;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class kakaoAPI {
     public static void main (String[] arg) {
-        String kakaoHost = "https://kauth.kakao.com";
-        String clientID = "a813d8398ab31e43715bd43a36673c37";
-        String tokenRequestUrl = kakaoHost + "/oauth/authorize?client_id="+ clientID +"&redirect_uri={redirect_uri}&response_type=code HTTP/1.1";
+        String kakaoHost = "https://kapi.kakao.com";
+        String token = "Bearer " + "kDi_Q8-O5mUB1ksvbWQcGS4CmdjXfP6IAOrtRQo9cpgAAAFzYCA3iw";
+        String sendMsgRequestUrl = kakaoHost + "/v2/api/talk/memo/default/send";
+        String sendMessage = "OkHttpClient 너무좋네;; 개사기;;";
+        JSONObject sendObject = new JSONObject();
+        JSONObject urlObject = new JSONObject();
+        urlObject.put("web_url", "https://finance.naver.com");
+        urlObject.put("mobile_web_url", "https://m.stock.naver.com");
+        sendObject.put("object_type", "text");
+        sendObject.put("text", sendMessage);
+        sendObject.put("link", urlObject);
+        sendObject.put("button_title", "눌러도 안나옴");
 
+        System.out.println("sendObject = " + sendObject.toString());
+        try {
+            OkHttpClient client = new OkHttpClient();
+            MediaType mediaType = MediaType.parse("application/json");
+            RequestBody body = RequestBody.create(mediaType, sendObject.toString());
+            HttpUrl.Builder httpBuilder = HttpUrl.get(new URL(sendMsgRequestUrl)).newBuilder();
+            httpBuilder.addQueryParameter("template_object", sendObject.toString());
+            Request request = new Request.Builder()
+                    .url(httpBuilder.build())
+                    .method("POST", body)
+                    .addHeader("Authorization", token)
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+            System.out.println(client.newCall(request).execute().body().string());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
